2fe61f7580cb8bf65944dfc2b57d6b7b669a646d
“Jilli <jilly@linder.org>
2023-11-29 10:02:15 -0700

    Fixed Code
    Hopefully this it and im done :(

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ const path = require("path");
 module.exports = {
     entry: "./src/app.js",
     mode: "production",
+    performance: { // disable warnings about bundle size
+        hints: false,
+    },    
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        publicPath: "/", // set a "public" path that you can navigate to in your browser - this lets "fetch" grab the file from the server
     },
     module: {
         rules: [
